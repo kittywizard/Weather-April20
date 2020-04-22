056011ef899adf3de4a7538b53e30f5eb41bbfec
marleigh <volcana@gmail.com>
2020-04-21 23:04:57 -0700

    JS comments

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("test");
+// Things to Do
+
+// Grab input from the form
+    // Based on mouse click??
+    // verify that it's in proper format
+// API call - grab info
+
+const input = document.getElementById('input');
+
+// mouse click ??
+
