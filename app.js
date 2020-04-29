e61ba9ae3c926e0d59848d662fb7c3c25b5d18bb
marleigh <volcana@gmail.com>
2020-04-28 17:12:05 -0700

    JS updates

--- app.js
+++ app.js
@@ -7,15 +7,28 @@
 
 const inputButton = document.getElementById('button');
 
+
 inputButton.addEventListener('click', function(e) {
   
     const input = document.getElementById('input').value;
     //test it for length, html doesn't seem to work well
     if(input.length != 5){
-        // you suck! try again
+        //better error
         console.log("oops");
+
+        const div = document.createElement('div');
+        div.className = 'alert alert-danger';
+
+        div.appendChild(document.createTextNode("Please try again"));
+                //get parent
+                const container = document.querySelector('.mainInput');
+        
+                //insert alert
+                container.insertBefore(div, container);
     }
-    console.log(input);
+    else {
+        console.log(input);
+    }
     //you need this to prevent the weird page reloading ???
     e.preventDefault();
 });
